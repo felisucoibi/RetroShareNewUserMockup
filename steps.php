0eeb2b3552fd7d36f9f00aa5d2a0705381a1043e
felisucoibi <info@justdust.es>
2016-11-29 21:28:47 +0100

    step 2 done

--- steps.php
+++ steps.php
@@ -179,9 +179,54 @@ This text is not part of the mockup but to comment things. To make it simple, no
 // STEP2 USER AND PASSWORD INPUT
 else if($step == "step2")
 {
+?>
+<html>
 
-	echo "step2";
+<head>
 
+<TABLE width="30%" align="center" border="1">
+<TR>
+<TD>
+<BR>
+<BR>
+<BR>
+<BR>
+<CENTER>STEP 2/4 USER AND PASSWORD</CENTER><br>
+<script>
+
+function Login(form) { username = new Array("username goes here"); password = new Array("password goes here"); page = "Name of html file to open when you push log in goes here" + ".html"; if (form.username.value == username[0] && form.password.value == password[0] || form.username.value == username[1] && form.password.value == password[1] ||
+
+form.username.value == username[2] && form.password.value == password[2] || form.username.value == username[3] && form.password.value == password[3] ||
+
+form.username.value == username[4] && form.password.value == password[4] || form.username.value == username[5] && form.password.value == password[5] ||
+
+form.username.value == username[6] && form.password.value == password[6] || form.username.value == username[7] && form.password.value == password[7] ||
+
+form.username.value == username[8] && form.password.value == password[8] || form.username.value == username[9] && form.password.value == password[9]) { self.location.href = page; } else { alert("Either the username or password you entered is incorrect.\nPlease try again."); form.username.focus(); } return true; }
+
+</script> </head> <body bgcolor="#eeeeee"> 
+<form action="" method="get">
+
+ <br> <center> <h5>Username: <input type="text" name="username" style="background:#bfbfbf;color:#212121;border-color:#212121;" onFocus="this.style.background = '#ffffff';"
+
+onBlur="this.style.background = '#bfbfbf';"> <br> Password: <input type="password" name="password" style="background:#bfbfbf;color:#212121;border-color:#212121;" onFocus="this.style.background = '#ffffff';"
+
+onBlur="this.style.background = '#bfbfbf';"> <br>  </form> </body> </html>	
+
+
+  <input type="hidden" name="step" value="step3">
+  <button type="submit" formmethod="post" formaction="">NEXT</button>
+</form>
+<BR><BR><BR>
+</TD>
+</TR>
+</TABLE>
+<p>
+<BR>
+Next step only user and password, the user is going to be used as a default identity too and as location "newuser" and is hidden for the user.
+</p>
+
+<?php
 }
 
 // STEP3 USER AND PASSWORD INPUT
@@ -193,5 +238,7 @@ else if($step == "step2")
 }
 
 ?>
+
+
 
 
